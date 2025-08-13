50480bab74a62d0a3f7c2da5f2006cf57022e990
David Thrane Christiansen <david@davidchristiansen.dk>
2025-08-13 09:14:44 +0200

    chore: more examples

---
+++ generate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Generating example blog HTML..."
+pushd blog
+lake exe generate-blog
+popd
+
+echo "Generating example package documentation HTML..."
+pushd package-docs/manual
+lake exe docs
+popd
+
+echo "Generating textbook HTML and code..."
+pushd textbook
+lake exe textbook
+cd _out
+zip -r code.zip example-code
+popd
+
+echo "Collecting generated HTML..."
+cp -r blog/_site out/blog
+cp -r package-docs/manual/_out/html-multi out/package-docs
+cp -r textbook/_out/html-multi out/textbook
+cp textbook/_out/code.zip out/textbook/
+
