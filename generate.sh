ba5987a3de6077b22b811a58ff72f8ffee132f37
David Thrane Christiansen <david@davidchristiansen.dk>
2025-08-13 13:37:49 +0200

    try more

--- generate.sh
+++ generate.sh
@@ -18,6 +18,7 @@ zip -r code.zip example-code
 popd
 
 echo "Collecting generated HTML..."
+mkdir out
 cp -r blog/_site out/blog
 cp -r package-docs/manual/_out/html-multi out/package-docs
 cp -r textbook/_out/html-multi out/textbook
